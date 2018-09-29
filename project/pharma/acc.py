9cb22d1d85fdb495aed7beb6f0928ae78aff2ae9
Samuel <ferraxi@gmail.com>
2018-09-29 02:41:23 +0200

    'improved'

--- project/pharma/acc.py
+++ project/pharma/acc.py
@@ -2,20 +2,18 @@
 import math
 
 class Hellberg:
-	def __init__(self, scales, normfactor):
-		self.scales = {}
-		for aa,z1,z2,z3 in [entry.split('\t') for entry in scales.split('\n')]:
-			self.scales[aa] = float(z1),float(z2),float(z3)
+	def __init__(self, scales, normfactor = 1):
+		self.scales = {aa: (float(z1),float(z2),float(z3)) for aa,z1,z2,z3 in [entry.split('\t') for entry in scales.split('\n')]}
 		self.p = normfactor
 		
-	def ac(self, sequence, z, lag):
+	def ac(self, sequence, z=0, lag=1):
 		n = len(sequence)
 		s = sum([self.scales[sequence[i]][z] * self.scales[sequence[i+lag]][z] for i in range(n-lag)])
 		return s/math.pow(n-lag, self.p)
 	
 	
 
-	def cc(sequence, za,zb, lag):
+	def cc(sequence, za,zb=0, lag=1):
 		n = len(sequence)
 		s = sum([self.scales[sequence[i]][za] * self.scales[sequence[i+lag]][zb] for i in range(n-lag)])
 		return s/math.pow(n-lag, self.p)
--- project/pharma/acc_shell.py
+++ project/pharma/acc_shell.py
@@ -3,18 +3,18 @@ import acc
 
 SCALE_FILE = 'hellberg.zsc'
 
-hellberg = acc.Hellberg(open(SCALE_FILE).read().strip(),1)
+hellberg = acc.Hellberg(open(SCALE_FILE).read().strip())
 
 #Examples from the book: Introduction to pharmaceutical bioinformatics, Wikberg
 sequence = 'VFWLFIY'
-print(sequence, hellberg.ac(sequence, 0, 1), sep = ' : ' )
+print(sequence, hellberg.ac(sequence), sep = ' : ' )
 
 sequence = 'ACDEFGI'
-print(sequence, hellberg.ac(sequence, 0, 1), sep = ' : ' )
+print(sequence, hellberg.ac(sequence), sep = ' : ' )
 
 #Assignment Pharmaceutical bioinformatics, Fall 2018
 sequence = 'ACDEFHI'
-print(sequence, hellberg.ac(sequence, 0, 1), sep = ' : ' )
+print(sequence, hellberg.ac(sequence), sep = ' : ' )
 
 sequence = 'VFWLYFI'
-print(sequence, hellberg.ac(sequence, 0, 1), sep = ' : ' )
+print(sequence, hellberg.ac(sequence), sep = ' : ' )

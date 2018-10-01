bbe1f228fca29f2a3402b307fe3c515a1780e708
FOU common <ferraxi@gmail.com>
2018-10-01 15:19:20 +0200

    littleman

--- project/alignment/superman_shell.py
+++ project/alignment/superman_shell.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python3
+
 import superman as al
 
-sm = al.Superman()
+sm = al.Littleman()
 
-print( sm.equals(1.0,2.0) )
+aseq = [1,2,3,4,5,6,7,8,20,34]
+bseq = [3,4,5,6,7,8,9,0,10,20,34]
+
+for a,b in sm.alignment(aseq,bseq):
+	print(*a, sep='\t')
+	print(*b, sep='\t')
+	print()

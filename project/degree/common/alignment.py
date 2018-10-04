641473bed6c6bbcbdf7c4fd84fbb3762609255fb
Samuel <ferraxi@gmail.com>
2018-10-04 23:53:33 +0200

    'realmen'

--- project/degree/common/alignment.py
+++ project/degree/common/alignment.py
@@ -1,10 +1,14 @@
 import threading
 import time
 import math
+import copy
 
 
 class Tool:
 	GAP = ''
+	
+	def score_to_string(score):
+		return '\n'.join(['\t'.join(['{:2.0f}'.format(s) for s in row]) for row in score])
 
 	def match_bonus(a,b):
 		return math.exp(-abs(a-b))
@@ -38,7 +42,13 @@ class Tool:
 
 
 	def addgap(gaps,i):
-		gaps[i] = gaps.get(i,0) + 1
+		prevgap = gaps.get(i+1,0)
+		if prevgap>0:
+			gaps[i] = prevgap + 1
+			del gaps[i+1]
+		else:
+			gaps[i] = gaps.get(i,0) + 1
+
 
 	def nextpair(gen1, gen2):
 		try:
@@ -107,7 +117,7 @@ class Aligner:
 				m[-1].append(0)
 		return m
 
-	def init_score(self, m,n):
+	def create_score(self, m,n):
 		score = self.zeros((m+1,n+1))
 		for i in range(0, m + 1):
 			score[i][0] = self.gap_penalty * i
@@ -115,6 +125,22 @@ class Aligner:
 			score[0][j] = self.gap_penalty * j
 
 		return score
+	
+
+	def init_score(self, seq1, seq2, m,n):
+		score = self.create_score(m,n)
+
+		for i in range(1, m + 1):
+			for j in range(1, n + 1):
+				a,b = seq1[self.pos1 + i - 1], seq2[self.pos2 + j -1]
+
+				match = score[i - 1][j - 1] + self.match_score(a, b, self.epsilon)
+				delete = score[i - 1][j] + self.gap_penalty
+				insert = score[i][j - 1] + self.gap_penalty
+				score[i][j] = max(match, delete, insert)
+
+		return score
+            
 
 	def equals(self, a,b, epsilon):
 		return type(a) is float and type(b) is float and abs(a-b) <= epsilon
@@ -130,7 +156,6 @@ class Aligner:
 
 	def isgap(self, i, gaps1, gaps2):
 		return len([1 for j,n in gaps1 if j<=i and i<=j+n])>0 or len([1 for j,n in gaps2 if j<=i and i<=j+n])>0
-
 
 
 	def numbers(self, seq1, seq2, gaps1,gaps2):
@@ -158,27 +183,14 @@ class Aligner:
 
 
 		return (float(identity) / count) * 100 if count > 0 else 0.0, score
-
+        
 
 class Realman(Aligner):
-	def align(self, seq1, seq2):
-		m,n = self.size1, self.size2
 
-		score = self.init_score(m, n)
-
-		for i in range(1, m + 1):
-			for j in range(1, n + 1):
-				a,b = seq1[self.pos1 + i - 1], seq2[self.pos2 + j -1]
-
-				match = score[i - 1][j - 1] + self.match_score(a, b, self.epsilon)
-				delete = score[i - 1][j] + self.gap_penalty
-				insert = score[i][j - 1] + self.gap_penalty
-				score[i][j] = max(match, delete, insert)
-
-		# Traceback and compute the alignment
+	def solution(self, seq1, seq2, score, m,n):
 		gaps1, gaps2 = {},{}
-		i,j = m,n # start from the bottom right cell
-		while i > 0 and j > 0: # end toching the top or the left edge
+		i,j = m,n 
+		while i > 0 and j > 0:
 			a,b = seq1[self.pos1 + i -1], seq2[self.pos2 + j -1]
 
 			score_current = score[i][j]
@@ -196,14 +208,84 @@ class Realman(Aligner):
 				Tool.addgap(gaps1,j-1)
 				j -= 1
 
-		# Finish tracing up to the top left cell
+		# Reach the top left cell
 		if j>0:
 			gaps1[0] = j
 		if i>0:
 			gaps2[0] = i
+			
+		
+		return gaps1, gaps2
 
+	def align(self, seq1, seq2):
+		m,n = self.size1, self.size2
+
+		score = self.init_score(seq1, seq2, m, n)
+
+		gaps1,  gaps2 = self.solution(seq1, seq2, score, m,n)
+		
 		return self.numbers(seq1, seq2, gaps1,gaps2), gaps1, gaps2
 
+class Realmen(Aligner):
+
+	def solution(self, seq1, seq2, score, stack):
+		i,j, gaps1, gaps2 = stack.pop()
+		while i > 0 and j > 0:
+			a,b = seq1[self.pos1 + i -1], seq2[self.pos2 + j -1]
+
+			score_current = score[i][j]
+			score_diagonal = score[i-1][j-1]
+			score_up = score[i][j-1]
+			score_left = score[i-1][j]
+			
+			diagonal, left = False, False
+
+			if score_current == score_diagonal + self.match_score(a, b, self.epsilon):
+				i -= 1
+				j -= 1
+				diagonal = True
+			
+			if score_current == score_left + self.gap_penalty:
+				if diagonal:
+					cgaps = copy.deepcopy(gaps2)
+					Tool.addgap(cgaps,i-1)
+					stack += [(i-1,j, copy.deepcopy(gaps1), cgaps)] # put that direction on hold
+				else:
+					Tool.addgap(gaps2,i-1)
+					i -= 1
+					left = True
+			
+			if score_current == score_up + self.gap_penalty:
+				if diagonal or left: # put that direction on hold
+					cgaps = copy.deepcopy(gaps1)
+					Tool.assgap(cgaps, j-1)
+					stack += [(i,j-1, cgaps, copy.deepcopy(gaps2))]
+				else:
+					Tool.addgap(gaps1,j-1)
+					j -= 1
+
+		# Reach the top left cell
+		if j>0:
+			gaps1[0] = j
+		if i>0:
+			gaps2[0] = i
+			
+		return gaps1, gaps2
+		
+	def solutions(self, seq1, seq2, score, m,n):
+		stack = [(m,n, {},{})]
+		while len(stack):
+			yield self.solution(seq1, seq2, score, stack)
+
+	def alignment(self, seq1, seq2):
+		m,n = self.size1, self.size2
+
+		score = self.init_score(seq1, seq2, m, n)
+		
+		for gaps1,  gaps2 in self.solutions(seq1, seq2, score, m,n):
+			yield self.numbers(seq1, seq2, gaps1,gaps2), gaps1, gaps2
+
+		
 class Realwater(Aligner):
 	def align(self,seq1, seq2):
 		m, n = self.size1, self.size2

d165c6075aaf4857d2787b6aaa35c32e45292575
samuel ferrer <ferraxi@gmail.com>
2019-03-14 18:09:02 +0100

    mutation

--- homework/workbench/report_0010/app.js
+++ homework/workbench/report_0010/app.js
@@ -11,5 +11,6 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"))
 app.get("/epsilon", (req, res) => res.sendFile(__dirname + "/epsilon.html"))
 app.get("/heatmap", (req, res) => res.sendFile(__dirname + "/heatmap.html"))
 app.get("/heatmapzero", (req, res) => res.sendFile(__dirname + "/heatmapzero.html"))
+app.get("/heatmapmut", (req, res) => res.sendFile(__dirname + "/heatmapmutation.html"))
 
 http.listen(PORT, () => console.log(`listening on ${PORT} ...`))
